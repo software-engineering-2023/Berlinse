b3ada2c95a5be443f827b8aae5d6816866a10293
Hemdoo <hemdoo@Ahmads-Mac.local>
2023-05-29 23:54:45 +0200

    Client page

---
+++ BcustomerMangement.js
@@ -0,0 +1,24 @@
+
+function showCustomerOptions() {
+    var customerOptions = document.getElementById("customerOptions");
+    if (customerOptions.style.display === "none") {
+        customerOptions.style.display = "block";
+    } else {
+        customerOptions.style.display = "none";
+    }
+}
+
+function viewCustomerDetails() {
+    // Logic to navigate to the page for viewing customer details
+    console.log("View Customer Details");
+}
+
+function viewContactInformation() {
+    // Logic to navigate to the page for viewing contact information
+    console.log("View Contact Information");
+}
+
+function viewAccountStatus() {
+    // Logic to navigate to the page for viewing account status
+    console.log("View Account Status");
+}
